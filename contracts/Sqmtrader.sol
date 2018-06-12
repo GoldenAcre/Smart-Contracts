319e2368453b68c4345ff9f8d691e51d034ae9fe
jeremias <grenzebach@pm.me>
2018-06-12 11:10:57 +0300

    Created a trader contract
    It receives SQM from the minter and sells it for a changeable price against ETH

---
+++ contracts/Sqmtrader.sol
@@ -0,0 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+pragma solidity 0.4.24;
+
+/// @title ERC777 SquareMetre Token (SQM) Contract
+/// @author Jeremias Grenzebach, GoldenAcre B.V.
+/// @dev This token contract is based on ERC777, pre-standard and ERC20 compatible.
